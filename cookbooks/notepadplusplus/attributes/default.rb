119279fa33b749f795edd60e37bf30773f42796b
LukeDrew-CodeComputrerLove <luke.drew@codecomputerlove.com>
2014-08-18 15:54:41 +0100

    Using librarian chef to get cookbooks
    Now just IIS

--- cookbooks/notepadplusplus/attributes/default.rb
+++
@@ -1,2 +0,0 @@
-default['notepadplusplus']['url']          = "http://download.tuxfamily.org/notepadplus/5.9.8/npp.5.9.8.Installer.exe"
-default['notepadplusplus']['package_name'] = "Notepad++"
--- cookbooks/notepadplusplus/recipes/default.rb
+++
@@ -1,4 +0,0 @@
-windows_package node['notepadplusplus']['package_name'] do
-  source node['notepadplusplus']['url']
-  action :install
-end
